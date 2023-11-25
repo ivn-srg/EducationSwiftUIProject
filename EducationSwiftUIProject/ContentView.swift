ef57149b6c083a251690049281406cc553db1756
ivn-srg <126597189+ivn-srg@users.noreply.github.com>
2023-11-25 21:44:05 +0600

    Initial Commit

---
+++ EducationSwiftUIProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EducationSwiftUIProject
+//
+//  Created by Sergey Ivanov on 25.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EducationSwiftUIProject/EducationSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducationSwiftUIProjectApp.swift
+//  EducationSwiftUIProject
+//
+//  Created by Sergey Ivanov on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct EducationSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
